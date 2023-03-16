3bb8b90427cb7ff9999fcfa5e692f14dc81a3248
Juan <juanmc015@gmail.com>
2023-03-16 12:04:26 +0100

    fixed form name

--- src/main/java/acme/forms/CompanyDashboard.java
+++ src/main/java/acme/forms/CompanyDashboard.java
@@ -1,13 +1,15 @@
 
 package acme.forms;
 
+import java.util.Map;
+
 import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class PracticumDashboard extends AbstractForm {
+public class CompanyDashboard extends AbstractForm {
 
 	//Serialisation Identifier
 
