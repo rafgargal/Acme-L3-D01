f8249d3df94a5e3647ee38cd42c082a456a936b0
jualeomad <jualeomad@alum.us.es>
2023-03-17 16:30:55 +0100

    feat: Banner entity and sample data

---
+++ src/main/java/acme/entities/banner/Banner.java
@@ -0,0 +1,60 @@
+
+package acme.entities.banner;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@PastOrPresent
+	protected Date				instantiationOrUpdateMoment;
+
+	@NotNull
+	@Temporal(value = TemporalType.TIMESTAMP)
+	protected Date				displayPeriodStart;
+
+	@NotNull
+	@Temporal(value = TemporalType.TIMESTAMP)
+	protected Date				displayPeriodEnd;
+
+	@URL
+	@NotBlank
+	protected String			pictureLink;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			slogan;
+
+	@NotBlank
+	@URL
+	protected String			webDocLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
