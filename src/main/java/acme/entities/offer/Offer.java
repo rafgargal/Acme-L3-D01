a8636c83c8b987c17fdf5d8fa9731d870a1352f5
JCLPV <80278014+JCLPV@users.noreply.github.com>
2023-03-17 19:41:46 +0100

    Arreglado offer y creado offer.csv

--- src/main/java/acme/entities/offer/Offer.java
+++ src/main/java/acme/entities/offer/Offer.java
@@ -3,11 +3,9 @@ package acme.entities.offer;
 
 import java.util.Date;
 
-import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -15,9 +13,14 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.framework.components.accounts.Administrator;
+import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Offer extends AbstractEntity {
 
 	protected static final long	serialVersionUID	= 1L;
@@ -28,24 +31,22 @@ public class Offer extends AbstractEntity {
 
 	@NotBlank
 	@Length(max = 75)
-	protected String			title;
-
-	@NotBlank
-	@Length(max = 75)
-	protected String			author;
+	protected String			heading;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			message;
+	protected String			summary;
 
-	@Email
-	protected String			email;
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				availabilityPeriodInit;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				availabilityPeriodFin;
+
+	@NotNull
+	protected Money				price;
 
 	@URL
 	protected String			link;
 
-	@Valid
-	@NotNull
-	@ManyToOne(optional = false)
-	protected Administrator		administrator;
 }
