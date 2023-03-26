b298cff0fea3ef6539d66fd681a36cd4295da524
rafgargal <rafgargal@alum.us.es>
2023-03-26 13:47:23 +0200

    AuthenticatedBulletin repository, services and controller created

---
+++ src/main/java/acme/features/authenticated/bulletin/AuthenticatedBulletinRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.authenticated.bulletin;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.bulletin.Bulletin;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AuthenticatedBulletinRepository extends AbstractRepository {
+
+	@Query("select b from Bulletin b where b.id = :id")
+	Bulletin findOneBulletinById(int id);
+
+	@Query("select b from Bulletin b")
+	List<Bulletin> findAllBulletin();
+
+}
