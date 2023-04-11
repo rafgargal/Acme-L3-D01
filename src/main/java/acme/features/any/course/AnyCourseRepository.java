b181c96c350e7d82cf20c0a02e875a9570860b89
rafgargal <rafgargal@alum.us.es>
2023-04-11 14:24:53 +0200

    AnyCourse repository, service and controller

---
+++ src/main/java/acme/features/any/course/AnyCourseRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.any.course;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.course.Course;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnyCourseRepository extends AbstractRepository {
+
+	@Query("select c from Course c where c.id = :id")
+	Course findOneCourseById(int id);
+
+	@Query("select c from Course c")
+	List<Course> findAllCourse();
+
+}
