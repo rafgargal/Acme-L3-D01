2a88deccdceb1bfdbd75f044880707603e5f132e
jualeomad <jualeomad@alum.us.es>
2023-07-10 17:36:41 +0200

    fixes

--- src/main/java/acme/entities/banner/Banner.java
+++ src/main/java/acme/entities/banner/Banner.java
@@ -8,7 +8,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -30,7 +30,7 @@ public class Banner extends AbstractEntity {
 
 	@NotNull
 	@Temporal(value = TemporalType.TIMESTAMP)
-	@PastOrPresent
+	@Past
 	protected Date				instantiationOrUpdateMoment;
 
 	@NotNull
